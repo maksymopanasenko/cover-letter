f5109ba608711363d848df2dc625f400a12b2e24
maksymopanasenko <maxpanas008@gmail.com>
2023-11-29 00:29:10 +0100

    gather all in one

--- backend/index.js
+++ backend/index.js
@@ -4,9 +4,49 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 require('dotenv').config();
+const _ = require("lodash");
 
 
-const routes = require("./src/routes/index.js");
+// const routes = require("./src/routes/index.js");
+// const Statistic = require('./src/models/Statistic.js');
+// const queryCreator = require('./src/commonHelpers/queryCreator.js');
+// const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const StatisticSchema = new Schema(
+  {
+    city: {
+      type: String,
+      required: true
+    },
+    country: {
+      type: String,      
+      required: true
+    },
+    count: {
+      type: Number,
+      required: true
+    }
+  },
+  { strict: false }
+);
+
+module.exports = Statistic = mongoose.model("statistics", StatisticSchema);
+
+
+function queryCreator(data) {
+  return Object.keys(data).reduce((queryObject, param) => {
+    if (isJSON(data[param])) {
+      queryObject[param] = JSON.parse(data[param]);
+    } else if (
+      !excludedParams.includes(param)
+    ) {
+      queryObject[param] = data[param];
+    }
+
+    return queryObject;
+  }, {});
+}
 
 const app = express();
 
@@ -22,10 +62,48 @@ mongoose
   .catch((err) => console.log(err));
 
 
-app.use('/api/statistics', routes);
+// app.use('/api/statistics', routes);
 app.get('/', (req, res) => {
   res.send('Hey this is my API running 🥳')
 });
+
+app.post('/', (req, res, next) => {
+    Statistic.findOne({ city: req.body.city })
+      .then((statistic) => {
+        if (!statistic) {
+          const initialQuery = _.cloneDeep(req.body);
+          initialQuery.count = 1;
+  
+          const newStatistic = new Statistic(queryCreator(initialQuery));
+  
+          newStatistic
+            .save()
+            .then((statistic) => res.json(statistic))
+            .catch((err) =>
+              res.status(400).json({
+                message: `Error happened on server: "${err}"`,
+              })
+            );
+        } else {
+          Statistic.findOneAndUpdate(
+            { city: req.body.city },
+            { $inc: { count: 1 } },
+            { new: true }
+          )
+            .then((cart) => res.json(cart))
+            .catch((err) =>
+              res.status(400).json({
+                message: `Error happened on server: "${err}" `,
+              })
+            );
+        }
+      })
+      .catch((err) =>
+        res.status(400).json({
+          message: `Error happened on server: "${err}" `,
+        })
+      );
+  });
 
 const port = process.env.PORT || 4000;
 
