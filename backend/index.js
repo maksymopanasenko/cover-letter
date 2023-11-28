035f03f63b15cd9ec368602b8224f7f27bb09ef6
maksymopanasenko <maxpanas008@gmail.com>
2023-11-29 00:20:33 +0100

    refresh code

--- backend/index.js
+++ backend/index.js
@@ -23,15 +23,9 @@ mongoose
 
 
 app.use('/api/statistics', routes);
-
-// if (process.env.NODE_ENV === 'production') {
-
-//   app.use(express.static('client/build'));
-
-//   app.get('*', (req, res) => {
-//     res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
-//   });
-// }
+app.get('/', (req, res) => {
+  res.send('Hey this is my API running 🥳')
+});
 
 const port = process.env.PORT || 4000;
 
