c8ee7b27566ea43f1f7933b6bf84cd9d679cf82b
maksymopanasenko <maxpanas008@gmail.com>
2023-11-28 23:03:53 +0100

    add cors

--- backend/index.js
+++ backend/index.js
@@ -10,7 +10,13 @@ const statistics = require('./src/routes/statistics');
 
 const app = express();
 
-app.use(cors());
+// app.use(cors());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 
 app.use(bodyParser.urlencoded({ extended: false }));
