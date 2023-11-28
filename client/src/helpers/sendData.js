284f9309d3dd32d82a785435771e72839fc438e6
maksymopanasenko <maxpanas008@gmail.com>
2023-11-28 21:30:21 +0100

    change configs

--- client/src/helpers/sendData.js
+++ client/src/helpers/sendData.js
@@ -1,6 +1,6 @@
 const sendDataToServer = async (data) => {
     try {
-        const response = await fetch('http://127.0.0.1:4000/api/statistics', {
+        const response = await fetch('https://cover-letter-api.vercel.app/api/statistics', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
